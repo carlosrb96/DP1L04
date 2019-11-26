4d2117bc9c69e64d883f6acb069eb2edf950117f
Jose Manuel <jmanuellt8@gmail.com>
2019-11-26 12:16:22 +0100

    Employer list and show jobs.
    Funcional

--- src/main/population/create-schema.sql
+++ src/main/population/create-schema.sql
@@ -107,6 +107,15 @@
         primary key (`id`)
     ) engine=InnoDB;
 
+    create table `employer` (
+       `id` integer not null,
+        `version` integer not null,
+        `user_account_id` integer,
+        `company` varchar(255),
+        `sector` varchar(255),
+        primary key (`id`)
+    ) engine=InnoDB;
+
     create table `garrido_bulletin` (
        `id` integer not null,
         `version` integer not null,
@@ -141,6 +150,21 @@
         `name` varchar(255),
         `sector` varchar(255),
         `stars` integer,
+        primary key (`id`)
+    ) engine=InnoDB;
+
+    create table `job` (
+       `id` integer not null,
+        `version` integer not null,
+        `deadline` datetime(6),
+        `description` varchar(255),
+        `final_mode` bit not null,
+        `more_info` varchar(255),
+        `reference` varchar(255),
+        `salary_amount` double precision,
+        `salary_currency` varchar(255),
+        `title` varchar(255),
+        `employer_id` integer not null,
         primary key (`id`)
     ) engine=InnoDB;
 
@@ -235,6 +259,9 @@
 
     insert into `hibernate_sequence` values ( 1 );
 
+    alter table `job` 
+       add constraint UK_7jmfdvs0b0jx7i33qxgv22h7b unique (`reference`);
+
     alter table `offer` 
        add constraint UK_iex7e8fs0fh89yxpcnm1orjkm unique (`ticker`);
 
@@ -263,6 +290,16 @@
        add constraint FK_6cyha9f1wpj0dpbxrrjddrqed 
        foreign key (`user_account_id`) 
        references `user_account` (`id`);
+
+    alter table `employer` 
+       add constraint FK_na4dfobmeuxkwf6p75abmb2tr 
+       foreign key (`user_account_id`) 
+       references `user_account` (`id`);
+
+    alter table `job` 
+       add constraint `FK3rxjf8uh6fh2u990pe8i2at0e` 
+       foreign key (`employer_id`) 
+       references `employer` (`id`);
 
     alter table `provider` 
        add constraint FK_b1gwnjqm6ggy9yuiqm0o4rlmd 
--- src/main/population/drop-schema.sql
+++ src/main/population/drop-schema.sql
@@ -15,6 +15,14 @@
        drop 
        foreign key FK_6cyha9f1wpj0dpbxrrjddrqed;
 
+    alter table `employer` 
+       drop 
+       foreign key FK_na4dfobmeuxkwf6p75abmb2tr;
+
+    alter table `job` 
+       drop 
+       foreign key `FK3rxjf8uh6fh2u990pe8i2at0e`;
+
     alter table `provider` 
        drop 
        foreign key FK_b1gwnjqm6ggy9yuiqm0o4rlmd;
@@ -41,6 +49,8 @@
 
     drop table if exists `customization`;
 
+    drop table if exists `employer`;
+
     drop table if exists `garrido_bulletin`;
 
     drop table if exists `guerrero_bulletin`;
@@ -48,6 +58,8 @@
     drop table if exists `horrillo_bulletin`;
 
     drop table if exists `investor_record`;
+
+    drop table if exists `job`;
 
     drop table if exists `lobato_bulletin`;
 
